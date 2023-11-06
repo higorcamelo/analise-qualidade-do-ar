730c3f547149dc5ce66870edb21bfad3061e8c79
Higor Camelo <higorcamelo@gmail.com>
2023-11-06 18:46:56 -0300

    Importação do Shapefile de subdivisões

--- dashboard.py
+++ dashboard.py
@@ -7,6 +7,14 @@ def main():
     Este script lê um arquivo geojson contendo dados de qualidade do ar e os exibe em um mapa do Streamlit.
     """
     gdf = gpd.read_file('data/processado/geo_data.geojson')
+    mapa_subdivisoes = gpd.read_file('data/processado/mapa_subdivisoes.shp')
+    mapa_subdivisoes.crs = "EPSG:4326"
+    gdf.crs = "EPSG:4326"
+
+    gdf = gdf.to_crs(epsg=4326)
+    mapa_subdivisoes = mapa_subdivisoes.to_crs(epsg=4326)
+
+    gdf = gpd.sjoin(gdf, mapa_subdivisoes, how="inner", predicate="intersects")
 
     st.title('Dashboard de dados da qualidade do ar')
 
